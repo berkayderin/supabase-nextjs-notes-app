bba14ce0226a47e296b8abb40c822de20e063967
Berkay Derin <zewodi@gmail.com>
2025-03-05 23:23:43 +0300

    feat: add notes table component and display notes

--- notes/app/notes/page.tsx
+++ notes/app/notes/page.tsx
@@ -1,6 +1,21 @@
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import React from 'react'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import { Trash } from 'lucide-react'
+
+type Note = {
+	id: number
+	title: string
+}
 
 const NotesPage = async () => {
 	const supabase = await createClient()
@@ -15,7 +30,30 @@ const NotesPage = async () => {
 		return redirect('/sign-in')
 	}
 
-	return <pre>{JSON.stringify(notes, null, 2)}</pre>
+	return (
+		<Table className="border">
+			<TableHeader>
+				<TableRow>
+					<TableHead>ID</TableHead>
+					<TableHead>Başlık</TableHead>
+					<TableHead>İşlemler</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{notes?.map((note: Note) => (
+					<TableRow key={note.id}>
+						<TableCell>{note.id}</TableCell>
+						<TableCell>{note.title}</TableCell>
+						<TableCell>
+							<Button variant="ghost" size={'sm'}>
+								<Trash size={16} />
+							</Button>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
+	)
 }
 
 export default NotesPage
